399191c20cac25a9cf43199a989023d25272fb0f
Akshay Shinde <akshay shinde>
2022-09-24 22:30:32 +0530

    engagement added

--- src/Homepage/UserEngagement.js
+++ src/Homepage/UserEngagement.js
@@ -1,15 +1,16 @@
 import React from 'react'
+import "./UserEngagement.css"
 
 const UserEngagement = () => {
   return (
     <div>
-      <h1>Hiiii User</h1>
-      <div>      
-        <input type={"textbox"} placeholder="What do you want ?"></input>
+      {/* <h1>Hiiii User</h1> */}
+      <div className='all'>      
+        <input className='input' type={"textbox"} placeholder="What do you want ?"></input>
         <br></br>
-        <button>Ask</button>
-        <button>Answer</button>
-        <button>Post</button>
+        <button className='btn'>Ask</button>
+        <button className='btn'>Answer</button>
+        <button className='btn'>Post</button>
       </div>
 
     </div>
