23aa33f304cc02ceabde2d99cf6fdafccd663ecc
Shashank <shashank08090@gmail.com>
2022-10-06 17:46:19 +0530

    bootstrap implemented

--- src/Homepage/UserEngagement.js
+++ src/Homepage/UserEngagement.js
@@ -2,11 +2,13 @@ import React from "react";
 import "./UserEngagement.css";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 const UserEngagement = () => {
   const [question, setQuestion] = useState("");
   const [ques, showques] = useState("");
-  
+
   const navigate = useNavigate();
 
   const postData = async () => {
@@ -26,58 +28,72 @@ const UserEngagement = () => {
     localStorage.setItem("user", JSON.stringify(result));
     showques(question);
   };
-  const postPhoto = (e)=>{
+  const postPhoto = (e) => {
     console.log("post  clk");
-  }
-  const answer = () =>{
+  };
+  const answer = () => {
     console.log("answer clk");
-    navigate("/question")
-  }
-const imageUpload = async(e)=>{
-  console.log("imag pload");
-  console.log(e.target.files[0]);
-  const files = e.target.files
-  const formData = new FormData()
-  const payload={"name":question,
-          "testImage":formData
-}
-  formData.append('testImage', files[0])
-  await fetch("http://localhost:5000/upload", {
-    method: 'POST',
-    body: payload,
-    // headers :{
-    //   "Content-Type": "multipart/form-data",
-    // },
-  });
-  // image1 = await image1.json();
-}
+    navigate("/question");
+  };
+  const imageUpload = async (e) => {
+    console.log("imag pload");
+    console.log(e.target.files[0]);
+    const files = e.target.files;
+    const formData = new FormData();
+    const payload = { name: question, testImage: formData };
+    formData.append("testImage", files[0]);
+    await fetch("http://localhost:5000/upload", {
+      method: "POST",
+      body: payload,
+      // headers :{
+      //   "Content-Type": "multipart/form-data",
+      // },
+    });
+    // image1 = await image1.json();
+  };
   return (
     <div>
       {/* <h1>Hiiii User</h1> */}
-      <div className="all">
-        <input
-          className="input"
-          type={"textbox"}
-          value={question}
-          onChange={(e) => {
-            setQuestion(e.target.value);
-            showques("");
-          }}
-          placeholder="What do you want ?"
-        ></input>
-        <br></br>
-        <button onClick={postData} className="btn">
-          Ask
-        </button>
-        <button onClick={answer} className="btn">Answer</button>
-        <button className="btn">Post</button>
-      </div>
+      <Container className="all" fluid>
+        <Row>
+          <Col>
+            {" "}
+            <input
+              className="input"
+              type={"textbox"}
+              value={question}
+              onChange={(e) => {
+                setQuestion(e.target.value);
+                showques("");
+              }}
+              placeholder="What do you want ?"
+            ></input>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            {" "}
+            <button onClick={postData} className="btn">
+              Ask
+            </button>
+          </Col>
+          <Col>
+            <button onClick={answer} className="btn">
+              Answer
+            </button>
+          </Col>
+          <Col>
+            <button className="btn">Post</button>
+          </Col>
+        </Row>
+      </Container>
       <div>
-        <h4>{ques} <button className="addPhoto">Add photo</button></h4>
+        <h4>
+          {ques} <button className="addPhoto">Add photo</button>
+        </h4>
       </div>
       <div className="photo">
-         
-        <input type={"file"} name={"testImage"} onChange={imageUpload}/>
+        <input type={"file"} name={"testImage"} onChange={imageUpload} />
         <button onClick={postPhoto}>Upload Photo</button>
       </div>
     </div>
